e51e6b692c6a7bc62c7facd619637c2c55468427
h2 <harshitjain0080@gmail.com>
2025-10-14 19:38:02 +0530

    add templates and url

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Products, Customer, Cart, CartItem, Order
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Customer)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Products
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import DetailView
 # Create your views here.
+def productList(request) :
+    products = Products.objects.all()
+    print(products)
+    return render(request ,store/productList.html , {'product' : products} )
+
+class ProductDetail(DetailView) :
+    model = Product 
+    context_object_name = 'product' 
+    template_name = 'product_detail.html'
