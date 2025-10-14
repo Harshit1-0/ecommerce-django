6dc65616b61e16d894eab3068cc75002849f05cf
h2 <harshitjain0080@gmail.com>
2025-10-14 18:33:25 +0530

    complete the model.py

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Category(models.Model) :
     slug = models.SlugField(unique = True)
     def __str__(self) :
         return self.name 
-class products(models.Model) :
+class Products(models.Model) :
     category = models.ForeignKey(Category, on_delete=models.CASCADE , related_name = 'products')
     name = models.CharField(max_length=250)
     slug = models.SlugField(unique =True)
@@ -21,14 +21,35 @@ class products(models.Model) :
         return self.name
 
 
-class customer(models.Model):
+class Customer(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
     addresses = models.TextField()
     phone = models.CharField(max_length=20, blank=True)
 
-    __str__(self):
+    def __str__(self):
         return self.user.username 
 
 
 class Cart(models.Model) :
+    customer = models.ForeignKey(Customer , on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+class CartItem(models.Model) :
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
     
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+    
+
+class Order(models.Model) :
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id}"
