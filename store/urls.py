36d2c9c473c582c834bfbae955f9d43e0af0faa7
h2 <harshitjain0080@gmail.com>
2025-10-15 18:11:26 +0530

    Add category-based product filtering via dropdown menu
    - Implemented category filter view using slug-based URLs
    - Updated template to use a Bootstrap dropdown for category selection
    - Added URL pattern and dynamic filtering logic using class-based views
    - Enhanced UX by showing current category title in product list
    
    This allows users to view products by selected category, improving navigation and search.

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('productlist/' , views.productList , name ='productList' ) ,
-    path('product/<int:pk>' , views.ProductDetail.as_view() , name = 'detailView')
+    path('product/' , views.productList , name ='productList' ) ,
+    path('product/<int:pk>' , views.ProductDetail.as_view() , name = 'detailView') ,
+    path('product/category/<slug:slug>' , views.ProductListByCategory.as_view() , name='productByCat')
 ]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products , Category
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import DetailView
+from django.views.generic import DetailView , ListView
 # Create your views here.
 def productList(request) :
     products = Products.objects.all()
+    cat = Category.objects.all()
+  
+    print(cat)
     print(products)
-    return render(request ,'store/productList.html' , {'products' : products} )
+  
+    return render(request ,'store/productList.html' , {'products' : products , 'cat' : cat} )
 
 class ProductDetail(DetailView) :
     model = Products 
     context_object_name = 'product' 
     template_name = 'store/product_detail.html'
+
+class ProductListByCategory(ListView):
+    model = Products
+    def get_queryset(self):
+        category_slug = self.kwargs.get('slug')  # assuming 'slug' in URL pattern
+        if category_slug:
+            return Products.objects.filter(category__slug=category_slug)
+        else:
+            return Products.objects.all()
+    # context_object_name = 'products'
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context['cat'] = Category.objects.all()
+        return context
+    
+
+    context_object_name = 'products'
+    template_name = 'store/productList.html'
+
+
 
